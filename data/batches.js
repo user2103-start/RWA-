8a5933ca231eef019768cc880cac4958c7e74f6e
user2103-start <prenu6019@gmail.com>
2026-05-14 18:54:36 +0530

    Create batches.js

---
+++ data/batches.js
@@ -0,0 +1,11 @@
+export const batches = [
+  { id: "534", name: "UP (GET) GNM 2026 - संजीवनी बैच", category: "nursing", price: "1999", thumbnail: "https://appx-content-v2.classx.co.in/paid_course3/2026-04-29-0.8697063953564991.jpeg" },
+  { id: "531", name: "COBRA बैच ( CRPF भर्ती 2026 CONSTABLE & TRADESMAN )", category: "defence", price: "1499", thumbnail: "https://appx-content-v2.classx.co.in/paid_course3/2026-04-30-0.762317168158011.jpeg" },
+  { id: "532", name: "IIT JEE 2027 Foundation (परिवर्तन बैच)", category: "engineering", price: "2999", thumbnail: "https://appx-content-v2.classx.co.in/paid_course3/2026-05-01-0.1304535364665007.jpeg" },
+  { id: "545", name: "NDA 2/2026 ( लक्ष्य बैच )", category: "defence", price: "2499", thumbnail: "https://appx-content-v2.classx.co.in/paid_course3/2026-04-28-0.9033230730567875.jpeg" },
+  { id: "544", name: "प्रारम्भ 3.O NEET 2027", category: "medical", price: "2999", thumbnail: "https://appx-content-v2.classx.co.in/paid_course3/2026-04-28-0.9703330316291376.jpeg" },
+  { id: "529", name: "राजस्थान CET 10+2, Graducation (नींव बैच)", category: "railway", price: "999", thumbnail: "https://appx-content-v2.classx.co.in/paid_course3/2026-04-16-0.8159316496207744.jpeg" },
+  { id: "533", name: "Class 11th Bihar Board 2027 ( तेजस बैच )", category: "school", price: "799", thumbnail: "https://appx-content-v2.classx.co.in/paid_course3/2026-04-23-0.162910291174862.jpeg" },
+  { id: "528", name: "Airforce Phase-2 1/2027 Astra Online बैच", category: "defence", price: "1999", thumbnail: "https://appx-content-v2.classx.co.in/paid_course3/2026-04-15-0.8157730050947272.jpeg" },
+  { id: "530", name: "UPSSSC Lower PCS 2026 ( अफसर बैच )", category: "ssc", price: "1299", thumbnail: "https://appx-content-v2.classx.co.in/paid_course3/2026-04-06-0.44485481500842416.jpeg" }
+];
